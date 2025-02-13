73ba74db26841783dd8cff9d7eb19f2dd7aca859
lico <luisa.coelho@gft.com>
2025-02-13 09:01:26 -0300

    feat: added entity "Tasks"

---
+++ service.one/src/main/java/com/larrieu/service/tasks/entity/TasksEntity.java
@@ -0,0 +1,22 @@
+package com.larrieu.service.tasks.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class TasksEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private LocalDateTime dueDate;
+    private boolean notified;
+
+}
