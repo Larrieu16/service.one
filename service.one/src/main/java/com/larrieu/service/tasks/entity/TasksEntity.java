e8187e489ca532503a7fa1f1324b545784558ab7
lico <luisa.coelho@gft.com>
2025-02-13 10:09:19 -0300

    fix: entity task

--- service.one/src/main/java/com/larrieu/service/tasks/entity/TasksEntity.java
+++ service.one/src/main/java/com/larrieu/service/tasks/entity/TasksEntity.java
@@ -14,7 +14,7 @@ public class TasksEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    private String email;
     private String title;
     private LocalDateTime dueDate;
     private boolean notified;
